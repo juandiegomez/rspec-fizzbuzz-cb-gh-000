19c4b0da41f13aa8da745e8605763795468c1f0c
Juan Diego Gomez Peña <juandi.gomezp@utadeo.edu.co>
2019-08-06 02:51:03 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
-  if int % 3 == 0 # if the number int is divisible by 3
-    "Fizz" # Go fizz
-  end
+  if int % 3 == 0 && int % 5 == 0
+    return "FizzBuzz"
+  elsif int % 3 == 0
+      return "Fizz"
   elsif int % 5 == 0
-    "Buzz"
+    return "Buzz"
+  else
   end
-  elsif int % 3 == 0 && int % 5 == =
-  "FizzBuzz"
-  end
-end
+ end
