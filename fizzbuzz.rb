a1e72b015c8a92a5d7d10ac1f3d548402001c8b3
Juan Diego Gomez Peña <juandi.gomezp@utadeo.edu.co>
2019-08-06 02:48:53 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,4 +4,10 @@ def fizzbuzz(int)
   if int % 3 == 0 # if the number int is divisible by 3
     "Fizz" # Go fizz
   end
+  elsif int % 5 == 0
+    "Buzz"
+  end
+  elsif int % 3 == 0 && int % 5 == =
+  "FizzBuzz"
+  end
 end
